3c6f63ffa1f2bea19b4ad4d127d3bbb3aa988f6b
hrsonion <hrsonion2@gmail.com>
2018-07-20 19:31:35 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <dag-maker/>
+  </div>
+</template>
+
+<script>
+import DagMaker from './components/DagMaker'
+
+export default {
+  name: 'App',
+  components: {
+    DagMaker
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background: #f6f6f6;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
