d9c36c6f24431f5a45d20958a6a93b9eafcb1d52
hrsonion <hrsonion2@gmail.com>
2019-04-21 22:49:07 +0800

    chore: try to make github host my page...

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/07akioni/dag-maker/master/release',
+    assetsPublicPath: 'https://raw.githubusercontent.com/07akioni/dag-maker/master/release/',
 
     /**
      * Source Maps
