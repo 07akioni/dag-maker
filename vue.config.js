c7797649bd3ccc989eeeb89a79ebf3ddd6300907
hrsonion <hrsonion2@gmail.com>
2019-04-22 19:36:10 +0800

    doc: add new index.html served by oss

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://github-dag-maker.oss-cn-beijing.aliyuncs.com/'
+    : '/',
   chainWebpack: config => {
     config
       .plugin('monaco-editor')
+      .after('html')
       .use(MonacoWebpackPlugin, [{
         languages: ['javascript'],
         features: ['find']
